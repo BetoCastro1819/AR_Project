f4973e775deb625aad5c8757f0479f0c46b6f999
Beto <betocastro1819@gmail.com>
2018-08-21 12:48:36 -0300

    Player collides with walls

--- AR_UnityProject/Assets/Scripts/Player.cs
+++ AR_UnityProject/Assets/Scripts/Player.cs
@@ -8,11 +8,15 @@ public class Player : MonoBehaviour
 
     private Vector3 movement;
     private Vector3 lookAtDir;
+    private Rigidbody rb;
+    private bool canMove = false;
 
     private void Start()
     {
         movement = transform.position;
         lookAtDir = transform.position;
+        rb = GetComponent<Rigidbody>();
+        canMove = true;
     }
 
     void Update ()
@@ -20,23 +24,45 @@ public class Player : MonoBehaviour
         Movement();
 	}
 
+    private void FixedUpdate()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, this.transform.forward, out hit, 1f))
+        {
+            if (hit.collider.tag == "Wall")
+                canMove = false;
+        }
+        else
+            canMove = true;
+    }
+
     void Movement()
     {
+        //canMove = true;
+
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
         lookAtDir.x = transform.position.x + horizontal;
         lookAtDir.z = transform.position.z + vertical;
 
-        movement.x += horizontal * speed * Time.deltaTime;
-        movement.z += vertical * speed * Time.deltaTime;
+        if (canMove)
+        {
+            movement.x += horizontal * speed * Time.deltaTime;
+            movement.z += vertical * speed * Time.deltaTime;
+        }
 
         transform.LookAt(lookAtDir);
+
         transform.position = movement;
     }
 
     private void OnCollisionEnter(Collision obj)
     {
-        if (obj.collider.tag == )
+        /*
+        if (obj.collider.tag == "Wall")
+            canMove = false;
+        */
     }
 }
